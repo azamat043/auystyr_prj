61c1ec36345c0659b843d38b82619211bbbe5716
azamat043 <200103422@stu.sdu.edu.kz>
2024-04-07 12:02:32 +0500

    fixes

--- backend/auystyr_prj/settings.py
+++ backend/auystyr_prj/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['auystyr.kz', '127.0.0.1', 'localhost', '0.0.0.0', '93.183.84.230']
 
 
 # Application definition
